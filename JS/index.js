dc2b4fbf560e9f640e143ed8c35db53f264c713c
christopherayork <christopherayork1992@gmail.com>
2019-06-25 19:37:19 -0400

    working on javascript transition

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,16 @@
 // Start for main JS!
+
+function collapse() {
+    let landing = document.querySelector('.landing');
+    let login = landing.querySelector('.btn');
+    login.addEventListener('click', () => {
+        login.preventDefault();
+        landing.classList.toggle('landing-collapsed');
+        let goto = login.getAttribute('href');
+        setTimeout(function() {
+            window.location = goto;
+        }, 1000);
+    });
+}
+
+collapse();
