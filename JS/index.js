e94119646e8e7db16a90b8c6dd3366757b7e0833
christopherayork <christopherayork1992@gmail.com>
2019-06-26 12:41:02 -0400

    Fixed merge conflicts, added assets

--- JS/index.js
+++ JS/index.js
@@ -13,4 +13,32 @@ function collapse() {
     });
 }
 
-collapse();
+//collapse();
+
+function setSlides() {
+    window.addEventListener('keydown', (event) => {
+        let slides = document.querySelectorAll('[data-slidenum]');
+        if(slides.length < 1) return; //we don't wont to do this with no slides
+        let current = document.querySelector('.showing');
+        if(event.keyCode == '38') { // up arrow key
+            event.preventDefault();
+            let prevNum = +current.dataset.slidenum - 1;
+            if(prevNum < 1) return;
+            let prev = slides[prevNum - 1];
+            prev.scrollIntoView({'behavior': 'smooth', 'block': 'center'});
+            current.classList.toggle('showing');
+            prev.classList.toggle('showing');
+        }
+        else if(event.keyCode == '40') { // down arrow key
+            event.preventDefault();
+            let nextNum = +current.dataset.slidenum + 1;
+            if(nextNum > slides.length) return;
+            let next = slides[nextNum - 1];
+            next.scrollIntoView({'behavior': 'smooth', 'block': 'center'});
+            current.classList.toggle('showing');
+            next.classList.toggle('showing');
+        }
+    });
+}
+
+setSlides();
